e19293ec606e2ff6b76912c327bd92bba691cf0f
T Michael Smith <timothy.smith2@asurion.com>
2021-04-02 12:37:24 -0500

    removed imports

--- src/components/QuizView/QuizView.js
+++ src/components/QuizView/QuizView.js
@@ -8,11 +8,6 @@ import {
 import { Auth } from "aws-amplify";
 import shuffle from "array-shuffle";
 
-import QuizHeader from "./QuizHeader";
-import QuizCard from "./QuizCard";
-import QuizAnswers from "./QuizAnswers";
-import QuizNav from "./QuizNav";
-
 const useStyles = makeStyles({
   spinnerBox: {
     display: "flex",
